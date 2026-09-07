410e42ff8e7dc275258d3444928e3161f88b3ba0
HemantPrajapat8769 <kumharhemant68@gmail.com>
2026-09-07 18:59:59 +0530

    Updated project

--- server/utils/pdfGenerator.js
+++ server/utils/pdfGenerator.js
@@ -63,7 +63,7 @@ class PDFGenerator {
   // 🟦 Header with Company Info + Logo + INVOICE Label
   static addCompanyHeader(doc, companyInfo, billData = null) {
     const defaultCompany = {
-      name: "Quibix - Billing System",
+      name: "Qubix - Billing System",
       address: "123 Business Street, Tech City, TC 12345",
       phone: "+1 (555) 123-4567",
       email: "info@billingapp.com",
